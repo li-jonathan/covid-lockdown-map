01ce8716afc8c6684219183799c1fe13db57297a
li-jonathan <li.jonathancj@gmail.com>
2020-04-23 01:15:15 -0400

    removed unused code. cleaned index.html and script.js

--- script.js
+++ script.js
@@ -8,14 +8,27 @@ var gather_id = document.getElementById("gather");
 var quarantine_id = document.getElementById("quarantine");
 var duration_id = document.getElementById("duration");
 
+/**
+ * @author: Jonathan Li
+ */
+
+/**
+ * 
+ * @param {*} check 
+ */
 function quarantineCheck(check) {
     if (check == "NA") {
-        return "No explicit statewide order. Please consult the CDC page on " + "Social Distancing, Quarantine, and Isolation.".link("https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/social-distancing.html");
+        return "No explicit statewide order. Please consult the CDC page on " 
+        + "Social Distancing, Quarantine, and Isolation.".link("https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/social-distancing.html");
     } else {
         return check;
     }
 }
 
+/**
+ * 
+ * @param {*} state 
+ */
 function updateData(state) {
     statename_id.innerHTML = state.name
     travel_id.innerHTML = "Traveling: ".bold() + state.travel;
@@ -24,13 +37,17 @@ function updateData(state) {
     duration_id.innerHTML = "Duration: ".bold() + state.duration;
 }
 
+/**
+ * 
+ * @param {*} statename 
+ */
 async function accessData(statename) {
     await doc.useServiceAccountAuth(require('./client_secret.json'));
     await doc.loadInfo();
     const sheet = doc.sheetsByIndex[0]; 
 
+    // TODO: very slow. find new method for getting a row by name
     const rows = await sheet.getRows({limit: 51});
-
     for (var i = 0; i < 52; i++) {
         if (rows[i].name == statename) {
             updateData(rows[i]);
@@ -40,6 +57,9 @@ async function accessData(statename) {
 
 }
 
+/**
+ * 
+ */
 $("path").click(function(e) {
     var name = $(this).data('name');
     accessData(name);
